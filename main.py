2a4c4fc4effd24c9d04ef70c2bf79ff821830fbc
m-schochet <schoche4@msu.edu>
2025-08-04 14:39:58 -0500

    add main .py class and a single function, not completed yet

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+class Star(object):
+    """
+    A simulated Star that holds all the information needed to plot an example spectra.
+    
+    """
+
+    def __init__(self, spectral_class, evolutionary_state, metallicity):
+        """
+        Function that initializes our star and the three selected parameters needed to determined
+        which spectra is generated
+        """
+
+        self.spectral_class = spectral_class
+        self.evolutionary_state = evolutionary_state
+        self.metallicity = metallicity
+
+    def select_spectra(self):
+        """
+        This function selects a given spectra to plot given the instatiated values for the
+        star in question
+        """
+        spec_list = pd.read_csv('(#name of csv with all the sim spectra info#)')
+        subselect = spec_list[(str(spec_list['spectral class'])==str(self.spectral_class)) 
+                             & (str(spec_list['evolutionary state'])==str(self.evolutionary_state))
+                             & (str(spec_list['metallicity'])==str(self.metallicity))]
+        filepath = subselect['(#name of column holding file names)#']
+        spectra = pd.read_csv(filepath)
