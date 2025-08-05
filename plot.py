ec40a498614509b0fbbaf8b48cbf13b3dba5282a
kgozman6159 <34196425+kgozman6159@users.noreply.github.com>
2025-08-05 15:22:11 -0500

    plotting on 2 axes

--- plot.py
+++ plot.py
@@ -26,10 +26,10 @@ def plotter(spectra_tuple, save=False):
 
     (teff, logg, m_h) = [parameters[i] for i in range(3)]
 
-    fig, [ax1, ax2]  = plt.subplots(2, 1, figsize=(20, 6), layout='constrained')
+    fig, [ax1, ax2]  = plt.subplots(2, 1, figsize=(20, 10), layout='constrained')
     
     ax1.plot(wav_unnormed, flux_unnormed, color='blue', lw=0.5)
-    ax1.plot(wav_unnormed, flux_unnormed, color='r', lw=0.5)
+    ax2.plot(wav_normed, flux_normed, color='r', lw=0.5)
 
     fig.suptitle(f'Spectrum of T$_[eff]$={teff}, $\log$(g)={logg}, [M/H]={m_h} ')
     fig.supxlabel('Wavelength ($\AA$)')
