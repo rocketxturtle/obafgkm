59df21a40f3c7a2b4458e08d2811ab8166503f87
Amaya Sinha <60234838+rocketxturtle@users.noreply.github.com>
2025-08-06 09:46:54 -0500

    Update __init__.py
    addede description

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 print("You have successfully imported obafgkm!")
+print("The spectra in this package were synthesized from MARCS model atmospheres using Korg. Due to this are currently only available within [3000,8000] K. Future releases will aim to expand this range.")
