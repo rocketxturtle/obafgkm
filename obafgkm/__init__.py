5253f33d32904c8a12c6bb3eddf794613caed025
m-schochet <schoche4@msu.edu>
2025-08-06 18:08:29 -0500

    move init.py to correct place

--- obafgkm/__init__.py
+++ obafgkm/__init__.py
@@ -1,2 +1,5 @@
+from * import unnormalized_spectra
+from * import normalized_spectra
+from * import star_types.csv
 print("You have successfully imported obafgkm!")
 print("The spectra in this package were synthesized from MARCS model atmospheres using Korg. Due to this are currently only available within [3000,8000] K. Future releases will aim to expand this range.")
