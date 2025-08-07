c3c685e2436cb40c7eba8c4b89f3895237ea092b
Meir Schochet <meirschochet13@gmail.com>
2025-08-07 10:52:43 -0500

    Update __init__.py

--- obafgkm/__init__.py
+++ obafgkm/__init__.py
@@ -12,4 +12,4 @@ rcparams = os.path.join(DATADIR, "rcparams.txt")
 
 
 print("You have successfully imported obafgkm!")
-print("The spectra in this package were synthesized from MARCS model atmospheres using Korg.\n Due to this are currently only available within [3000, 8000] K. Future releases will aim to expand this range.")
+print("The spectra in this package were synthesized from MARCS model atmospheres using Korg. Due to this are currently only available within [3000, 8000] K. Future releases will aim to expand this range.")
