8b5006d578b9d1476071916c26fe6ba41de79032
Meir Schochet <meirschochet13@gmail.com>
2025-08-07 16:10:12 -0500

    Update conf.py

--- obafgkm/docs/conf.py
+++ obafgkm/docs/conf.py
@@ -35,7 +35,6 @@ release = '0.1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  "sphinx_rtd_theme",
   "sphinx.ext.autodoc",
   "sphinx.ext.doctest",
   "sphinx.ext.todo",
