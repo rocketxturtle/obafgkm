d72fa6f79be99f8626eab776350991cee9f9cb2b
Meir Schochet <meirschochet13@gmail.com>
2025-08-07 15:47:49 -0500

    Update main.py

--- obafgkm/main.py
+++ obafgkm/main.py
@@ -6,25 +6,32 @@ from obafgkm import unnormed_data, normed_data, STARCSV
 
 os.chdir(pathlib.Path.cwd())
 class Star(object):
-    """
-    A simulated Star that holds all the information needed to plot an example spectra.
-    
+    """The core class for obafgkm, which holds the parameters needed to choose the spectra for plotting 
+     
+     Attributes:
+        effective_temperature (int): KORG effective temperature for selected star (in Kelvin)
+        surface_gravity (float): KORG surface gravity for selected star (in dex, re: 10**surface_gravity)
+        metallicity (str): KORG metallicity for selected star ('solar','subsolar', & 'supersolar' with values of -2.0, 0.0, and 0.5 for [M/H]
+
     """
 
     def __init__(self, effective_temperature:int, surface_gravity:float, metallicity:str):
+        """__init__ method for our Star objects
+       
+        Args:
+            effective_temperature (int): KORG effective temperature for selected star (in Kelvin)
+            surface_gravity (float): KORG surface gravity for selected star (in dex, re: 10**surface_gravity)
+            metallicity (str): KORG metallicity for selected star ('solar','subsolar', & 'supersolar' with values of -2.0, 0.0, and 0.5 for [M/H]
         """
-        Function that initializes our star and the three selected parameters needed to determined
-        which spectra is generated
-        """
-
         self.effective_temperature = effective_temperature
         self.surface_gravity = surface_gravity
         self.metallicity = metallicity
 
     def select_spectra(self):
-        """
-        This function selects a given spectra to plot given the instatiated values for the
-        star in question
+        """this method selects the spectra file for plotting
+       
+        Args:
+            None
         """
         spec_list = pd.read_csv(STARCSV)
         subselect = spec_list[(spec_list['effective_temp']==self.effective_temperature) & (spec_list['surface_gravity']==self.surface_gravity) & (spec_list['metallicity']==self.metallicity)]
